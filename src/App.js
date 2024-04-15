21c55dd75cf3f115ea0ba9441126343a395a841f
Glebexe <53045081+Glebexe@users.noreply.github.com>
2024-04-15 14:50:58 +0300

    Lab7-8

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-env jquery */
+/* global $ */
 import logo from './logo.svg';
 import './App.css';
 import TextInputComponent from './TextInput';
@@ -11,123 +13,28 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-function App(){
+
+function App({data}){      
     return(
         <div className="App">
-            <span className="left">
-                <TextInputComponent/>
-                <MyCaruosel/>
-            </span>
             <span className="right">
-                <Search/>
+                <Search data = {data}/>
             </span>
         </div>
     );
 }
 
-const pics = ['https://sun9-58.userapi.com/impg/rSsXOGJBcFMumKt92EigXvUWUzJtxBU5NQadiQ/xGOcfJfdpwM.jpg?size=1358x906&quality=95&sign=1c482055c469b5b24e53eb15b6d785ed&c_uniq_tag=WjTKFC4PuhjztO1tI_KELrW3EGNPMFU4bIA-EUKWbJ8&type=album','https://webpulse.imgsmail.ru/imgpreview?mb=webpulse&key=pulse_cabinet-image-ad371779-5f70-4b90-9647-f5faf320ab1d','https://kartinkof.club/uploads/posts/2022-03/1648267735_47-kartinkof-club-p-khekhe-mem-kot-49.jpg']
 
-
-const list = [
-  {
-    name:'Porsche 911 Turbo',
-    power: 580,
-    gas_tank_volume: 67,
-    max_speed: 320,
-    fuel_consumption: 11.5,
-    image: "https://images.drive.ru/i/0/5e7b211aec05c4921c000014.jpeg"
-  },
-  {
-    name:'Mercedes-Benz C',
-    power: 150,
-    gas_tank_volume: 52,
-    max_speed: 245,
-    fuel_consumption: 7.0,
-    image: "https://aybaz.ru/wp-content/uploads/b/2/8/b282a365ceb9d1daff03673f66102b96.jpeg"  
-  },
-  {
-    name:'Lamborghini Gallardo',
-    power: 550,
-    gas_tank_volume: 90,
-    max_speed: 325,
-    fuel_consumption: 15.7,
-    image: "https://i.pinimg.com/originals/a6/47/3d/a6473d3a6f7180c65e13b10d2cf7b9d9.jpg"  
-  },
-  {
-    name:'Ford FSerieso',
-    power: 600,
-    gas_tank_volume: 120,
-    max_speed: 180,
-    fuel_consumption: 21.1,
-    image: "https://kopotic.com/wp-content/uploads/2014/03/11ForRapt_01.jpg"
-  },
-  {
-    name:'Chery Arrizo 8',
-    power: 186,
-    gas_tank_volume: 55,
-    max_speed: 205,
-    fuel_consumption: 6.2,
-    image: "https://cdn.motor1.com/images/mgl/1ZzmQ3/s1/chery-arrizo-8---china.jpg"
-  },
-  {
-    name:'Lada Largus',
-    power: 90,
-    gas_tank_volume: 60,
-    max_speed: 160,
-    fuel_consumption: 7.5,
-    image: "https://autocentr-khimki.ru/static/imgCar/688/2758.jpg?v=10.6"
-  },
-  {
-    name:'Smart Fortwo',
-    power: 71,
-    gas_tank_volume: 28,
-    max_speed: 151,
-    fuel_consumption: 4.1,
-    image: "https://sportishka.com/uploads/posts/2022-11/1667562335_61-sportishka-com-p-malenkii-mersedes-smart-vkontakte-64.jpg"
-  },
-  {
-    name:'УАЗ-452',
-    power: 55.2,
-    gas_tank_volume: 80,
-    max_speed: 95,
-    fuel_consumption: 13,
-    image: "https://carsweek.ru/upload/resize_cache/iblock/d67/1200_900_1/d67a50107f1cbbed232f54661cd6f5e6.jpg"
-  },
-
-];
-
-
-function MyCaruosel()
-{
-    return(
-        <div class="carousel" data-bs-theme="dark" variant="dark" style={{width: 600, height: 500, marginTop:200}}>          
-          <Carousel>
-            <Carousel.Item id="blueCarousel">
-                <img class="d-block w-100" src={pics[0]} alt="First slide"/>
-            </Carousel.Item>
-
-            <Carousel.Item id="greenCarousel">
-                <img class="d-block w-100" src={pics[1]} alt="Second slide"/>
-            </Carousel.Item>
-
-            <Carousel.Item id="pinkCarousel">
-                <img class="d-block w-100" src={pics[2]} alt="Third slide"/>
-            </Carousel.Item>
-          </Carousel>
-        </div>
-    );
-}
-
-function Search(){
-  const[searchTerm, setSearchTerm] = useState('');
-  const[minSpeedSearchTerm, setMinSpeedSearchTerm] = useState('');
-  const[maxSpeedSearchTerm, setMaxSpeedSearchTerm] = useState(10000);
-  
-  
-  const searchCards = list.filter(function (item){
-    return item.name.includes(searchTerm) && (item.max_speed > minSpeedSearchTerm) && (item.max_speed < maxSpeedSearchTerm);
-  });
+function Search({data}){
     
+  const[searchTerm, setSearchTerm] = useState('');
+  const[minRatingSearchTerm, setMinRatingSearchTerm] = useState('');
+  const[minMetacriticRatingSearchTerm, setMinMetacriticRatingSearchTerm] = useState('');
+
+    
+  const searchCards = data.filter(function (item){
+    return item.name.includes(searchTerm) && (item.rating > minRatingSearchTerm) && (item.metacritic > minMetacriticRatingSearchTerm);
+  });    
   
   
   const handleChange = (event) =>
@@ -135,31 +42,29 @@ function Search(){
     setSearchTerm(event.target.value);
   }
   
-  const handleMinSpeedChange = (event) =>
+  const handleMinRatingChange = (event) =>
   {
-    setMinSpeedSearchTerm(event.target.value);
+    setMinRatingSearchTerm(event.target.value);
   }
-  const handleMaxSpeedChange = (event) =>
+  const handleMinMetacriticRatingChange = (event) =>
   {
-    if (event.target.value == '')
-        setMaxSpeedSearchTerm(10000);
-    else
-        setMaxSpeedSearchTerm(event.target.value);
+    setMinMetacriticRatingSearchTerm(event.target.value);
   }
-  
+
   return(
-    <div>
+    <div class='p-3'>
       <h1>Поиск</h1>
-      <div>
-      <label htmlFor='search'>По имени:</label>
-      <input id="search" type='text' onChange={handleChange} style={{marginTop:"40px", marginBottom:"40px"}}></input>
+      <div class='w-100 p-3'>
+          <label htmlFor='search'>По имени:</label>
+          <input id="search" type='text' onChange={handleChange} style={{marginTop:"40px", marginBottom:"40px",}}></input>
       </div>
       <div>
-      <label htmlFor='search' style={{marginLeft:"10px"}}>Мин. скорость:</label>
-      <input id="search" type='text' onChange={handleMinSpeedChange} style={{marginTop:"40px", marginBottom:"40px"}}></input>
-      <label htmlFor='search' style={{marginLeft:"10px"}}>Макс. скорость:</label>
-      <input id="search" type='text' onChange={handleMaxSpeedChange} style={{marginTop:"40px", marginBottom:"40px"}}></input>
+          <label htmlFor='search' style={{marginLeft:"10px"}}>Мин. рейтинг:</label>
+          <input id="search" type='text' onChange={handleMinRatingChange} style={{marginTop:"40px", marginBottom:"40px"}}></input>
+          <label htmlFor='search' style={{marginLeft:"10px"}}>Мин. оценка на Метакритик:</label>
+          <input id="search" type='text' onChange={handleMinMetacriticRatingChange} style={{marginTop:"40px", marginBottom:"40px"}}></input>
       </div>
+      
       <ListCars list={searchCards}/>
     </div>
   )
@@ -167,61 +72,56 @@ function Search(){
 
 const ListCars = (props) =>{
   return(
-    <div>
-      <Row>
-        {props.list.map(function(item)
-        {
-          return <Col class="col-xl-3">
-            <Card>
-                <div class="card rounded bg-dark">
-                    <div class="card-image">
-                        <span><h5>{item.name}</h5></span>
-                        <img class="img-fluid" src={item.image}alt="Alternate Text" />
-                    </div>
-                    <div class="card-image-overlay m-auto">
-                      <table class="table table-noborder table-dark">
-                          <tbody>
-                            <tr>
-                              <td>
-                                <span calss="info"> 
-                                    <i class="bi bi-fuel-pump"></i> {item.gas_tank_volume} л/100 км
-                                </span>
-                                <div class='helpText'>Расход топлива</div>
-                              </td>
-                              <td>
-                                <span calss="info"> 
-                                    <i class="bi bi-speedometer"></i> {item.max_speed} км/ч
-                                </span>
-                                <div class='helpText'>Макс. скорость</div>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>
-                                <span calss="info"> 
-                                    <i class="bi bi-rocket-takeoff-fill"></i> {item.power} л.с.
-                                      </span>
-                                <div class='helpText'>Мощность</div>
-                              </td>
-                              <td>
-                                <span calss="info"> 
-                                    <i class="bi bi-slash-square-fill"></i> {item.gas_tank_volume} л
-                                            </span>
-                                <div class='helpText'>Объем бензобака</div>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
+      <div class='myDiv'>
+        <div class="row row-cols-lg-4 row-cols-md-2 row-cols-sm-1 g-4">      
+            {props.list.map(function(item)
+            {
+              return <div>
+                      <div class="card bg-dark text-light h-100">
+                          <img src={item.image} class="card-img-top" alt="..."/>
+                          <h2>{item.name}</h2>
+                          <div class="card-body d-flex flex-column fs-5">
+                            <div class="card-image-overlay m-auto align-items-center">
+                              <table class="table table-noborder table-dark">
+                                  <tbody>
+                                    <tr>
+                                      <td>
+                                        <span calss="info"> 
+                                            <i class="bi bi-star-fill"></i> {item.rating}/5
+                                        </span>
+                                        <div class='helpText'>Рейтинг</div>
+                                      </td>
+                                      <td>
+                                        <span calss="info"> 
+                                            <i class="bi bi-journal-check"></i> {item.metacritic}/100
+                                        </span>
+                                        <div class='helpText'>Метакритик</div>
+                                      </td>
+                                    </tr>
+                                    <tr>
+                                      <td>
+                                        <span calss="info"> 
+                                            <i class="bi bi-calendar4-event"></i> {item.released}
+                                              </span>
+                                        <div class='helpText'>Дата выхода</div>
+                                      </td>
+                                      <td>
+                                        <span calss="info"> 
+                                            <i class="bi bi-clock-fill"></i> {item.playtime} ч.
+                                                    </span>
+                                        <div class='helpText'>Время прохождения</div>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
 
-                    </div>
-                    <div class="card-body text-center">
-                        <button type="button" class="btn btn-outline-light">Подробнее</button>                     
-                    </div>
-                </div>   
-
-            </Card>
-          </Col>
-        })}
-      </Row>
+                            </div>
+                            <a href="#"><button type="button" class="btn btn-outline-light fs-4">Подробнее</button></a>
+                          </div>
+                      </div>
+                    </div>    
+            })}
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-env jquery */
+/* global $ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -5,10 +7,40 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
+let data = []
+const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://rawg-video-games-database.p.rapidapi.com/games?key=22c9553a0abd4274a922003e0c8d7f59',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'bc2d176e22mshac18e82dfcbfe75p1972e7jsn044986d1dd75',
+        'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+    }
+};
+
+
+$.ajax(settings).done(function (response) {
+    for(const game of response['results']){
+        let curData = {};
+        curData['name'] = game['name']
+        curData['metacritic'] = game['metacritic']
+        curData['rating'] = game['rating']
+        curData['released'] = game['released']
+        curData['image'] = game['short_screenshots'][0]['image']
+        curData['playtime'] = game['playtime']
+        curData['parent_platforms'] = game['parent_platforms']
+        curData['dominant_color'] = game['dominant_color']
+        data.push(curData)
+    }
+});
+console.log(data)
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data} />
   </React.StrictMode>
 );
 
